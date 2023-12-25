4782f13942371fc42a5c5d41e8003a2529e87035
LightBreaker <lightbreaker812@gmail.com>
2023-12-25 22:42:45 +0630

    Updated set creation

---
+++ Sets/set_creation/set_creation.py
@@ -0,0 +1,10 @@
+#Creation of sets
+##Using curly brackes
+set1={1,2,3}
+set2=set([3,4,5])
+setempty=set()
+print(f"Set1-->{set1}")
+print(f"Set2-->{set2}")
+print(f"This is empty set-->{setempty}")
+print("Type of set1",type(set1))
+print("Type of set2",type(set2))
