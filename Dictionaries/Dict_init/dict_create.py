6e320e3baa98adfd130f191ce4881b6ebd7c9d3b
LightBreaker <lightbreaker812@gmail.com>
2023-12-26 20:03:17 +0630

    Updated

--- Dictionaries/Dict_init/dict_create.py
+++ Dictionaries/Dict_init/dict_create.py
@@ -1,12 +1,17 @@
 #Creating Dictionaries
+##empty dictionary
 emptdict={}
+##Using {}
 dict1={"male":32,"female":40,"young":12,"old":13}
 dict2={"name":"Mg Mg","Gender":"Male","Age":23,"Salary per hour":23.33}
+##Using comprehension
 dict3={x:x**2 for x in range(11)}
 print("Empty dictionary",emptdict)
+##Using loops for printing
 print("--Dictionary1--")
 for x in dict1.keys():
     print(f"{x} : {dict1.get(x)}")
+##Using comprehension for printing
 print("--Dictionary2--")
 _=[print(f"{x}:{dict2.get(x)}") for x in dict2.keys()]
 print("--Dictionary3--")
